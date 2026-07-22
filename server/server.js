76a82bb9afe65588c5ab430c28e4ffb18c261f5d
Shivansh-developer <pandeyshivansh243@gmail.com>
2026-07-22 14:31:10 +0530

    Bind server to 0.0.0.0 for Railway compatibility

--- server/server.js
+++ server/server.js
@@ -11,4 +11,4 @@ sequelize.authenticate()
   .then(() => console.log('Tables synced'))
   .catch((err) => console.error('DB error:', err));
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
